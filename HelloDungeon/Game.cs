4545f33dbc143edd8a2292b83707fb9a2dc96fea
Hunter Rachal <142814960+TheTrueDodoKing@users.noreply.github.com>
2023-10-02 16:55:32 -0500

    practi ce

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -1,16 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace HelloDungeon
 {
     class Game
     {
+        float independence =4.8f;
+        double test2;
+        int lives = 3;
+        string name;
+        bool playerIsAlive = true;
+        char playerChoice;
+        double time;
+
+        int Smaller(int a, int b)
+        {
+            if (a <= b)
+            {
+                return a;
+            }
+            return b;
+        }
+        int FizzBizz(int i = 0; int <= 30; i++)
         public void Run()
         {
+            //int count = 1;
+            //while(count <= 10)
+            //{
+            //    Console.WriteLine(count++);
+            //}
+            //for (int i = 0; i <= 10; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
 
+            for (int i = 0; i <= 30; i++)
+            {
+                if(i += 3)
+                {
+
+                }
+            }
         }
     }
 }
